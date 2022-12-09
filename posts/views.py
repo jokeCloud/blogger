dfcf2cfa7f2a21a8ba01224081af2271b815d933
jokecloud <medalhaemerson@gmail.com>
2022-12-09 11:59:25 -0300

    class based view

--- posts/views.py
+++ posts/views.py
@@ -1,7 +1,8 @@
 from django.contrib import messages
 from django.db.models import Case, Count, Q, When
-from django.shortcuts import redirect
-from django.views.generic.edit import UpdateView
+from django.shortcuts import get_object_or_404, redirect, render
+from django.views import View
+# from django.views.generic.edit import UpdateView
 from django.views.generic.list import ListView
 
 from comentarios.forms import FormComentario
@@ -69,6 +70,43 @@ class PostCategoria(PostIndex):
         return qs
 
 
+class PostDetalhes(View):
+    template_name = 'posts/post_detalhes.html'
+
+    def setup(self, request, *args, **kwargs):
+        super().setup(request, *args, **kwargs)
+
+        pk = self.kwargs.get('pk')
+        post = get_object_or_404(Post, pk=pk, publicado_post=True)
+        self.contexto = {
+            'post': post,
+            'comentarios': Comentario.objects.filter(post_comentario=post,
+                                                     publicado_comentario=True),  # noqa
+            'form': FormComentario(request.POST or None),
+        }
+
+    def get(self, request, *args, **kwargs):
+        return render(request, self.template_name, self.contexto)
+
+    def post(self, request, *args, **kwargs):
+        form = self.contexto['form']
+
+        if not form.is_valid():
+            return render(request, self.template_name, self.contexto)
+
+        comentario = form.save(commit=False)
+
+        if request.user.is_authenticated:
+            comentario.usuario_comentario = request.user
+
+        comentario.post_comentario = self.contexto['post']
+        comentario.save()
+        messages.success(
+            request, 'Seu comentário foi salvo, estamos analisando.')
+        return redirect('post_detalhes', pk=self.kwargs.get('pk'))
+
+
+"""
 class PostDetalhes(UpdateView):
     template_name = 'posts/post_detalhes.html'
     model = Post
@@ -95,3 +133,4 @@ class PostDetalhes(UpdateView):
         messages.success(
             self.request, 'Comentário inserido com sucesso, aguarda avaliação.')  # noqa
         return redirect('post_detalhes', pk=post.id)
+"""
