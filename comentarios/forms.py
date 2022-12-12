cc35e0d4031af94b358d4daed7c44731cbab99df
jokecloud <medalhaemerson@gmail.com>
2022-12-12 07:49:09 -0300

    recaptcha

--- comentarios/forms.py
+++ comentarios/forms.py
@@ -1,3 +1,4 @@
+import requests
 from django.forms import ModelForm
 
 from .models import Comentario
@@ -5,8 +6,30 @@ from .models import Comentario
 
 class FormComentario(ModelForm):
     def clean(self):
-        data = self.cleaned_data
-        nome = data.get('nome_comentario')
+        raw_data = self.data
+        recaptcha_response = raw_data.get('g-recaptcha-response')
+
+        recaptcha_request = requests.post(
+            'https://www.google.com/recaptcha/api/siteverify',
+            data={
+                'secret': '6Lfey3IjAAAAANGHMt1jRzS7RIlU860URVU5_Udw',
+                'response': recaptcha_response
+            }
+        )
+        recaptcha_result = recaptcha_request.json()
+
+        if not recaptcha_result.get('success'):
+            self.add_error(
+                'comentario',
+                'Desculpe Mr. Robot, ocorreu um erro'
+            )
+
+        # https://www.google.com/recaptcha/api/siteverify
+        # secret key
+        # resposta captcha
+
+        cleaned_data = self.cleaned_data
+        nome = cleaned_data.get('nome_comentario')
         # email = data.get('email_comentario')
         # comentario = data.get('comentario')
 
